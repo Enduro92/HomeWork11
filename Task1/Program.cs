ec1ca97daaab924d4495663e83c5a3a966e1f507
Denis <den.paranoid1992@gmail.com>
2023-02-05 10:00:23 +0300

    решил первую задачу2

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -4,15 +4,11 @@
 //782 -> 8
 //918 -> 1
 
-Console.WriteLine("Введите трёхзначное число :");
-string numberStr = Console.ReadLine();
-int numberN = Convert.ToInt32(numberStr);
-Console.WriteLine(numberN % 10);
-
-Console.WriteLine("Введите двухзначное число");
+Console.WriteLine("Введите трехзначное число :");
 string number2Str = Console.ReadLine();
 string str = number2Str.ToString();
 Console.WriteLine(str[1]);
+
 
 
 
